6df2849fe8d8f857f6d8b472393b6c4f9a28d4b5
Vladislav Khvostov <vladkhvo@gmail.com>
2016-08-05 09:56:37 +0000

    WIP

---
+++ routes/photos.js
@@ -0,0 +1,18 @@
+const photos = [];
+photos.push({
+    name: 'Node.js Logo',
+    path: 'http://nodejs.org/images/logos/nodejs-green.png'
+});
+
+photos.push({
+    name: 'Rayan Speaking',
+    path: 'http://nodejs.org/images/ryan-speaker.jpg'
+});
+
+exports.lists = function (req, res) {
+    console.log('route photos exected');
+    res.render('photos', {
+        title: 'Photos',
+        photos
+    })
+}
